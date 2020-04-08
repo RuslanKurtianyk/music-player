c5fbe7cc0837adcd0724384be9c865473c7035d8
Kurtianyk, Ruslan <ruslan.kurtianyk@partner.cgm.com>
2020-04-08 22:26:44 +0300

    add vuetify support

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
 import Player from './Player.vue'
- 
+import vuetify from './plugins/vuetify';
+
 Vue.use(Vuetify)
- 
+
 new Vue({
   el: '#app',
+  vuetify,
   render: h => h(Player)
 })
