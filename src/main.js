d5c06b81a4a1032d094c18e0eaab5f5282e7e4d7
Kurtianyk, Ruslan <ruslan.kurtianyk@partner.cgm.com>
2020-04-08 22:33:00 +0300

    refactor: remvoe odd files

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
 import Player from './Player.vue'
 import vuetify from './plugins/vuetify';
-
-Vue.use(Vuetify)
 
 new Vue({
   el: '#app',
