e43e2559f9aba12a03240dee2211caf4efe09ae6
Kurtianyk, Ruslan <ruslan.kurtianyk@partner.cgm.com>
2020-04-09 22:19:07 +0300

    add base list of tracks

--- src/Player.vue
+++ src/Player.vue
@@ -3,6 +3,7 @@
     <v-content>
       <v-container>
         <player-title-bar></player-title-bar>
+        <player-playlist-panel :playlist="playlist"></player-playlist-panel>
       </v-container>
     </v-content>
   </v-app>
@@ -10,15 +11,30 @@
  
 <script>
 import PlayerTitleBar from './components/PlayerTitleBar.vue'
+import PlayerPlaylistPanel from './components/PlayerPlaylistPanel.vue'
+import { Howl } from 'howler'
 
   export default {
     components: {
-      PlayerTitleBar
+      PlayerTitleBar,
+      PlayerPlaylistPanel
     },
     data () {
       return {
- 
+        playlist: [
+          {title: 'Creeping Death', artist: 'Metallica', howl: null, display: true},
+          {title: 'Don\'t stop the party', artist: 'Black Eyed Peas', howl: null, display: true},
+          {title: 'My Humps', artist: 'Black Eyed Peas', howl: null, display: true},
+        ]
       }
+    },
+    created: function() {
+      this.playlist.forEach(track => {
+        let file = track.title.replace(/\s/g, "_")
+        track.howl = new Howl({
+          src: [`./playlist/${file}.mp3`]
+        })
+      })
     }
   }
 </script>
---
+++ src/components/PlayerPlaylistPanel.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card height="330">
+    <v-list>
+      <v-list-item
+        v-for="(track, index) in playlist"
+        :key="track.title"
+        v-show="track.display">
+        <v-list-item-content>
+          <v-list-item-title>{{ index }} {{ track.artist }} - {{ track.title }}</v-list-item-title>
+        </v-list-item-content>
+        <v-spacer></v-spacer>
+        {{ track.howl.duration() }}
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+ 
+<script>
+  export default {
+    props: {
+      playlist: Array
+    }
+  }
+</script>
